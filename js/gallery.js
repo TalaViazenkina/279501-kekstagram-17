8804b20eb1c1c6d85cab0df5262835458557cc4e
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-16 17:21:42 +0300

    Добавлена отрисовка массива фотографий

--- js/gallery.js
+++ js/gallery.js
@@ -4,9 +4,53 @@
 (function () {
   var data = []; // массив данных, полученных с сервера
 
+  var pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('a.picture');
+
+  var picturesContainer = document.querySelector('section.pictures');
+
+  /**
+  * отрисовывает фото
+  * @param {Object} obj
+  * @return {Element}
+  */
+  var renderPicture = function (obj) {
+    // клонируем шаблон
+    var picture = pictureTemplate.cloneNode(true);
+
+    // заполняем адрес фото
+    var pictureImg = picture.querySelector('.picture__img');
+    pictureImg.src = obj.url;
+
+    // вводим количество лайков
+    var pictureLike = picture.querySelector('.picture__likes');
+    pictureLike.textContent = obj.likes;
+
+    // вводим количество комментариев
+    var pictureComment = picture.querySelector('.picture__comments');
+    pictureComment.textContent = obj.comments.length;
+
+    return picture;
+  };
+
+  /**
+  * добавляет в разметку список фотографий
+  * @param {Array} arr
+  */
+  var renderPictureList = function (arr) {
+    var fragment = document.createDocumentFragment();
+
+    arr.forEach(function (it) {
+      fragment.appendChild(renderPicture(it));
+    });
+
+    picturesContainer.appendChild(fragment);
+  };
+
   var onSuccessLoad = function (response) {
     data = response;
-    console.log(data);
+    renderPictureList(data);
   };
 
   var onErrorLoad = function (message) {
