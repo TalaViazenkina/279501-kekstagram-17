e52b079c09ed5e97209abe6d969fdf7bdba8bb54
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-17 08:58:35 +0300

    Реализовано изменение размеров превью

--- js/form.js
+++ js/form.js
@@ -2,6 +2,12 @@
 
 // модуль формы редактирования фотографии
 (function () {
+  var ScaleValue = {
+    MIN: 25,
+    MAX: 100,
+    STEP: 25
+  };
+
   var form = document.querySelector('#upload-select-image');
 
   var fileUploadField = form.querySelector('#upload-file'); // поле загрузки фото
@@ -9,6 +15,11 @@
   var formCloseButton = form.querySelector('#upload-cancel'); // закрытие формы редактирования
 
   var preview = form.querySelector('.img-upload__preview img');
+
+  // управление размером
+  var scaleSmaller = form.querySelector('.scale__control--smaller');
+  var scaleBigger = form.querySelector('.scale__control--bigger');
+  var scaleInput = form.querySelector('.scale__control--value');
 
   /**
   * скрывает форму редактирования
@@ -55,6 +66,28 @@
     }
   };
 
+  /**
+  * увеличивает размер превью
+  */
+  var increaseSize = function () {
+    var currentValue = parseInt(scaleInput.value, 10);
+    currentValue += ScaleValue.STEP;
+
+    scaleInput.value = (currentValue <= ScaleValue.MAX) ? (currentValue + '%') : (ScaleValue.MAX + '%');
+    preview.style.transform = 'scale(' + (parseInt(scaleInput.value, 10) / 100) + ')';
+  };
+
+    /**
+  * уменьшает размер превью
+  */
+  var decreaseSize = function () {
+    var currentValue = parseInt(scaleInput.value, 10);
+    currentValue -= ScaleValue.STEP;
+
+    scaleInput.value = (currentValue >= ScaleValue.MIN) ? (currentValue + '%') : (ScaleValue.MIN + '%');
+    preview.style.transform = 'scale(' + (parseInt(scaleInput.value, 10) / 100) + ')';
+  };
+
   var onFormClick = function (evt) {
     if (evt.target.name === 'effect') {
       // удаляем с превью класс начинающийся с 'effects__preview--'
@@ -64,6 +97,10 @@
       if (evt.target.value !== 'none') {
         preview.classList.add('effects__preview--' + evt.target.value);
       }
+    } else if (evt.target === scaleBigger) {
+      increaseSize();
+    } else if (evt.target === scaleSmaller) {
+      decreaseSize();
     }
 
   };
