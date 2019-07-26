8a66eb5e034d774395c4a6088e575ac7f90b01d5
Natallia Viazenkina <natala.viaz@gmail.com>
2019-07-26 15:37:53 +0300

    Исправлена ошибка по критерию Д19

--- js/backend.js
+++ js/backend.js
@@ -5,6 +5,7 @@
   var URL_LOAD = 'https://js.dump.academy/kekstagram/data';
   var URL_SAVE = 'https://js.dump.academy/kekstagram';
   var TIMEOUT = 10000; // 10s
+  var STATUS_OK = 200; // Код ответа сервера 200
   window.backend = {
     isSaving: false, // флаг текущей отправки
     load: function (onSuccess, onError) {
@@ -13,7 +14,7 @@
       xhr.timeout = TIMEOUT;
 
       xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
+        if (xhr.status === STATUS_OK) {
           onSuccess(xhr.response);
         } else {
           onError('Ошибка загрузки данных. Статус ответа сервера: ' + xhr.status + ' ' + xhr.statusText);
@@ -38,7 +39,7 @@
       xhr.responseType = 'json';
 
       xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
+        if (xhr.status === STATUS_OK) {
           onSuccess(xhr.response);
         } else {
           onError('Статус ответа сервера: ' + xhr.status + ' ' + xhr.statusText);
